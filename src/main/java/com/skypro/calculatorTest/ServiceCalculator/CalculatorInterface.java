f0e6a4fb67514a516ed1fe2bcd78856a7d87f195
kila1 <kila14081996@gmail.com>
2024-07-08 22:13:37 +0300

    Тестирование

---
+++ src/main/java/com/skypro/calculatorTest/ServiceCalculator/CalculatorInterface.java
@@ -0,0 +1,12 @@
+package com.skypro.calculatorTest.ServiceCalculator;
+
+public interface CalculatorInterface {
+
+    String welcomeCalculator();
+
+    Integer plus(int value1, int value2);
+    Integer minus(int value1, int value2);
+    Integer multiple(int value1, int value2);
+    Integer divide(int value1, int value2);
+
+}
---
+++ src/test/java/com/skypro/calculatorTest/ServiceCalculatorParametrizedTest.java
@@ -0,0 +1,65 @@
+package com.skypro.calculatorTest;
+
+import com.skypro.calculatorTest.ServiceCalculator.ServiceCalculator;
+import com.skypro.calculatorTest.exeptions.DivideNotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class ServiceCalculatorParametrizedTest {
+
+    ServiceCalculator serviceCalculator = new ServiceCalculator();
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(10,38),
+                Arguments.of(0,6),
+                Arguments.of(150,5),
+                Arguments.of(100,3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void CheckingTheCalculatorForAddition(Integer value1, Integer value2) {
+        int answer = value1 + value2;
+
+        Integer result = serviceCalculator.plus(value1,value2);
+
+        Assertions.assertEquals(answer,result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void CheckingTheCalculatorForSubtraction(Integer value1, Integer value2) {
+        int answer = value1 - value2;
+
+        Integer result = serviceCalculator.minus(value1,value2);
+
+        Assertions.assertEquals(answer,result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void CheckingTheCalculatorForMultiply(Integer value1, Integer value2) {
+        int answer = value1 * value2;
+
+        Integer result = serviceCalculator.multiple(value1,value2);
+
+        Assertions.assertEquals(answer,result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void CheckingTheCalculatorForDivide(Integer value1, Integer value2) {
+        int answer = value1 / value2;
+
+        Integer result = serviceCalculator.divide(value1,value2);
+
+        Assertions.assertEquals(answer,result);
+    }
+
+}
